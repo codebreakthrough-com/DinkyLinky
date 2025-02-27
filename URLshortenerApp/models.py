98a3e24d32e568198842f537fb00f7cb384195af
Alessandro Ducci <duccialessandro001@gmail.com>
2025-02-27 20:18:14 +0400

    Patch: updated user in db to be nullable, ran makemigrations, tried fixing docker volume but still broken.

--- URLshortenerApp/models.py
+++ URLshortenerApp/models.py
@@ -6,7 +6,7 @@ class URL(models.Model):
     short_url = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     expiration_date = models.DateTimeField(null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.long_url
